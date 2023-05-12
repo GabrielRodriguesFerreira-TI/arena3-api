0bcfddb0ac62107ead9d88609b8727f9c34f948c
GabrielRf <ti.gabrielrf@gmail.com>
2023-05-11 21:14:51 -0300

    Feat: add all tests for create a user

--- src/__test__/integrations/users/createUser.route.spec.ts
+++ src/__test__/integrations/users/createUser.route.spec.ts
@@ -25,4 +25,167 @@ describe("POST /users", () => {
     await mongoose.disconnect();
     await server.stop();
   });
+
+  it("Success: Must be able to create a user - Full body", async () => {
+    const response = await request
+      .post(baseUrl)
+      .send(userCreateMock.userComplete);
+
+    const { password, ...bodyEqual } = userCreateMock.userComplete;
+
+    expect(response.status).toBe(201);
+    expect(response.body).toEqual(expect.objectContaining(bodyEqual));
+    expect(response.body).not.toEqual(
+      expect.objectContaining({ password: expect.any(String) })
+    );
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        _id: expect.any(String),
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+      })
+    );
+    expect(response.body.createdAt).toEqual(
+      expect.stringMatching(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/)
+    );
+    expect(response.body.updatedAt).toEqual(
+      expect.stringMatching(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/)
+    );
+  });
+
+  it("Success: Must be able to create a user - User 'addmin'", async () => {
+    const response = await request
+      .post(baseUrl)
+      .send(userCreateMock.userCompleteAdmin);
+
+    const { password, ...bodyEqual } = userCreateMock.userComplete;
+
+    expect(response.status).toBe(201);
+    expect(response.body).toEqual(expect.objectContaining(bodyEqual));
+    expect(response.body).not.toEqual(
+      expect.objectContaining({ password: expect.any(String) })
+    );
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        _id: expect.any(String),
+        isAdmin: true,
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+      })
+    );
+    expect(response.body.createdAt).toEqual(
+      expect.stringMatching(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/)
+    );
+    expect(response.body.updatedAt).toEqual(
+      expect.stringMatching(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/)
+    );
+  });
+
+  it("Error: Must not be able to create a user - Email already exists", async () => {
+    const uniqueUser = new User({
+      ...userCreateMock.userComplete,
+    });
+    await uniqueUser.save();
+
+    const response = await request
+      .post(baseUrl)
+      .send(userCreateMock.userUniqueEmail);
+
+    const expectResult = {
+      status: 409,
+      bodyMessage: {
+        message: 'Email "gabrielrf@gmail.com" already exists',
+      },
+    };
+
+    expect(response.status).toBe(expectResult.status);
+    expect(response.body).toStrictEqual(expectResult.bodyMessage);
+  });
+
+  it("Error: Must not be able to create a user - Username already exists", async () => {
+    const uniqueUser = new User({
+      ...userCreateMock.userComplete,
+    });
+
+    await uniqueUser.save();
+
+    const response = await request
+      .post(baseUrl)
+      .send(userCreateMock.userUniqueUsername);
+
+    const expectResults = {
+      status: 409,
+      bodyMessage: {
+        message: 'Username "A3on" already exists',
+      },
+    };
+
+    expect(response.status).toBe(expectResults.status);
+    expect(response.body).toStrictEqual(expectResults.bodyMessage);
+  });
+
+  it("Error: Must not be able to create a user - Invalid body", async () => {
+    const response = await request
+      .post(baseUrl)
+      .send(userCreateMock.userInvalidBody);
+
+    const expectResults = {
+      status: 400,
+      bodyMessage: {
+        message: {
+          username: ["String must contain at most 50 character(s)"],
+          email: [
+            "Invalid email",
+            "String must contain at most 50 character(s)",
+          ],
+          firstName: ["String must contain at most 50 character(s)"],
+          lastName: ["String must contain at most 50 character(s)"],
+        },
+      },
+    };
+
+    expect(response.status).toBe(expectResults.status);
+    expect(response.body).toStrictEqual(expectResults.bodyMessage);
+  });
+
+  it("Error: Must not be able to create a user - Invalid Types", async () => {
+    const response = await request
+      .post(baseUrl)
+      .send(userCreateMock.userInvalidBodyType);
+
+    const expectResults = {
+      status: 400,
+      bodyMessage: {
+        message: {
+          username: ["Expected string, received number"],
+          password: ["Expected string, received number"],
+          email: ["Expected string, received array"],
+          firstName: ["Expected string, received object"],
+          lastName: ["Expected string, received boolean"],
+        },
+      },
+    };
+
+    expect(response.status).toBe(expectResults.status);
+    expect(response.body).toStrictEqual(expectResults.bodyMessage);
+  });
+
+  it("Error: Must not be able to create a user - Invalid body required keys", async () => {
+    const response = await request
+      .post(baseUrl)
+      .send(userCreateMock.userInvalidKeys);
+
+    const expectResults = {
+      status: 400,
+      bodyMessage: {
+        message: {
+          email: ["Required"],
+          password: ["Required"],
+        },
+      },
+    };
+
+    expect(response.status).toBe(expectResults.status);
+    expect(response.body).toStrictEqual(expectResults.bodyMessage);
+  });
 });
